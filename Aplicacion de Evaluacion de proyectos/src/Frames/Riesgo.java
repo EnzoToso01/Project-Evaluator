baec558b23988baf293309d86c7a69655065ea5d
EnzoToso01 <enzogabrieltosoni@gmail.com>
2022-02-25 17:31:03 -0300

    Riesgo V2

--- Aplicacion de Evaluacion de proyectos/src/Frames/Riesgo.java
+++ Aplicacion de Evaluacion de proyectos/src/Frames/Riesgo.java
@@ -34,24 +34,122 @@ public class Riesgo extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        txtriesgos = new javax.swing.JLabel();
+        btn_añadirfila_riesg = new javax.swing.JButton();
+        btn_quitarfila_riesg = new javax.swing.JButton();
+        btn_guardar = new javax.swing.JButton();
+        scroll_riesg = new javax.swing.JScrollPane();
+        tabla_riesgos = new javax.swing.JTable();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setLocation(new java.awt.Point(0, 0));
-        setPreferredSize(new java.awt.Dimension(900, 690));
         setSize(new java.awt.Dimension(900, 690));
+
+        txtriesgos.setFont(new java.awt.Font("Bahnschrift", 0, 24)); // NOI18N
+        txtriesgos.setForeground(new java.awt.Color(240, 255, 255));
+        txtriesgos.setText("Riesgos");
+
+        btn_añadirfila_riesg.setText("añadir fila");
+        btn_añadirfila_riesg.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn_añadirfila_riesgActionPerformed(evt);
+            }
+        });
+
+        btn_quitarfila_riesg.setText("quitar fila");
+        btn_quitarfila_riesg.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn_quitarfila_riesgActionPerformed(evt);
+            }
+        });
+
+        btn_guardar.setText("Guardar");
+        btn_guardar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn_guardarActionPerformed(evt);
+            }
+        });
+
+        tabla_riesgos.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+                "Indicador"
+            }
+        ));
+        tabla_riesgos.setShowGrid(true);
+        scroll_riesg.setViewportView(tabla_riesgos);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 900, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(22, 22, 22)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(scroll_riesg, javax.swing.GroupLayout.PREFERRED_SIZE, 850, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(txtriesgos, javax.swing.GroupLayout.PREFERRED_SIZE, 197, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 288, Short.MAX_VALUE)
+                        .addComponent(btn_añadirfila_riesg)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(btn_quitarfila_riesg)
+                        .addGap(87, 87, 87)
+                        .addComponent(btn_guardar)
+                        .addGap(66, 66, 66))))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 690, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(44, 44, 44)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                        .addComponent(btn_guardar)
+                        .addComponent(btn_añadirfila_riesg)
+                        .addComponent(btn_quitarfila_riesg))
+                    .addComponent(txtriesgos, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addComponent(scroll_riesg, javax.swing.GroupLayout.DEFAULT_SIZE, 571, Short.MAX_VALUE)
+                .addGap(7, 7, 7))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void btn_añadirfila_riesgActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_añadirfila_riesgActionPerformed
+        // TODO add your handling code here
+        //Añade filas a ingresos
+        DefaultTableModel tblmodel = (DefaultTableModel) tabla_riesgos.getModel();
+        Vector<?> rowData = null;
+        tblmodel.addRow(rowData);
+    }//GEN-LAST:event_btn_añadirfila_riesgActionPerformed
+
+    private void btn_quitarfila_riesgActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_quitarfila_riesgActionPerformed
+        // TODO add your handling code here:
+        //Quita filas a idicadores, si no se selecciona una fila,se elimina la ultima
+        DefaultTableModel tblmodel = (DefaultTableModel) tabla_riesgos.getModel();
+        if (tabla_riesgos.getSelectedRowCount() == 1) {
+            tblmodel.removeRow(tabla_riesgos.getSelectedRow());
+        } else {
+            tblmodel.removeRow(tabla_riesgos.getRowCount() - 1);
+        }
+    }//GEN-LAST:event_btn_quitarfila_riesgActionPerformed
+
+    private void btn_guardarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_guardarActionPerformed
+        // TODO add your handling code here:
+        File indicadores = new File("C:\\Project evaluator\\indicadores.txt");
+        //Declaro la tabla por ser método privado y paso por parametro para el método exportar
+        JTable tabla = getTabla_indicadores();
+        try {
+            Tabla.exportar(indicadores, tabla);
+            JOptionPane.showMessageDialog(null, "datos guardados");
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Hubo un error al guardar los datos");
+        }
+    }//GEN-LAST:event_btn_guardarActionPerformed
 
     /**
      * @param args the command line arguments
@@ -89,5 +187,11 @@ public class Riesgo extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btn_añadirfila_riesg;
+    private javax.swing.JButton btn_guardar;
+    private javax.swing.JButton btn_quitarfila_riesg;
+    private javax.swing.JScrollPane scroll_riesg;
+    private javax.swing.JTable tabla_riesgos;
+    private javax.swing.JLabel txtriesgos;
     // End of variables declaration//GEN-END:variables
 }
